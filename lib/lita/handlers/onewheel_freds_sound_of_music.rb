e9c2fe0247381e8794ba0b3655bad9abdb2ae1f5
Andrew Kreps <andrew.kreps@gmail.com>
2016-09-05 01:50:54 -0700

    Yeah that

--- lib/lita/handlers/onewheel_freds_sound_of_music.rb
+++ lib/lita/handlers/onewheel_freds_sound_of_music.rb
@@ -1,7 +1,11 @@
 module Lita
   module Handlers
     class OnewheelFredsSoundOfMusic < Handler
-      # insert handler code here
+      route(/^freds\s+(.*)/i, :freds_search, command: true, help: 'freds whatevs')
+
+      def freds_search(response)
+        response.reply 'wat'
+      end
 
       Lita.register_handler(self)
     end
